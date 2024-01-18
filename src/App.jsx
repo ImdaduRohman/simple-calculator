baa21417ae9c1b07ebe4ce59d5450b1b0c1cdf59
ImdaduRohman <imdadu.ylabs1@gmail.com>
2024-01-18 23:43:18 +0700

    feat: redesign ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { CiDark, CiLight } from "react-icons/ci";
-import { FiDelete } from "react-icons/fi";
 import { useState } from 'react';
+import { RiDeleteBack2Line } from "react-icons/ri";
 import './App.css'
 
 function App() {
@@ -55,56 +55,55 @@ function App() {
     setToggleDarkMode(!toggleDarkMode);
   }
 
+  // bg-[#ffffdd] 
   return (
-    <div className='min-h-screen bg-[#ffffdd] dark:bg-[#252628]'>
-      <div className='bg-[#fff0d2] dark:bg-[#1b1c1e] p-4 border-b-[4px] border-[#483e2e] dark:border-[#fdfdfd]'>
-        <div className='flex gap-2'>
-          <input 
-            type='checkbox' 
-            id='toggle-dark-mode' 
-            className='hidden'
-            checked={toggleDarkMode || ''}
-            value={toggleDarkMode || ''}
-            onChange={handleToggleDarkMode}
-          />
-          <label htmlFor='toggle-dark-mode'>
-            <div className={`w-[48px] rounded-full p-[1px] cursor-pointer bg-white`}>
-              <div className='w-5 h-5 rounded-full bg-[#f1af60] toggle-circle flex justify-center items-center'>
-                {!toggleDarkMode ? <CiLight className='text-white font-medium' /> : <CiDark className='text-white font-medium' />}
-              </div>
+    <div className='bg-[#F3F2F2] dark:bg-[#283345] relative min-h-screen'>
+      <div className='flex items-center gap-4 p-4 absolute top-0 z-20'>
+        {/* <div onClick={handleToggleDarkMode}className={`w-8 h-8 rounded-full toggle-circle flex justify-center items-center ${!toggleDarkMode ? 'bg-black' : 'bg-white'}`}>
+          {!toggleDarkMode ? <CiLight className='text-white font-medium' size={22}/> : <CiDark className='text-black font-medium' size={25} />}
+        </div> */}
+        <input 
+          type='checkbox' 
+          id='toggle-dark-mode' 
+          className='hidden'
+          checked={toggleDarkMode || ''}
+          value={toggleDarkMode || ''}
+          onChange={handleToggleDarkMode}
+        />
+        <label htmlFor='toggle-dark-mode'>
+          <div className={`w-[78px] h-9 rounded-full cursor-pointer flex items-center ${!toggleDarkMode ? 'label-light' : 'label-dark'}`}>
+            <div className={`w-7 h-7 rounded-full toggle-circle flex justify-center items-center ml-[5px] ${!toggleDarkMode ? 'bg-white' : 'bg-black'}`}>
+              {!toggleDarkMode ? <CiLight className='text-black font-medium' size={22}/> : <CiDark className='text-white font-medium' size={25}/>}
             </div>
-          </label>
-          <div className='text-[#8c9298]'>Switch to {toggleDarkMode ? 'Light' : 'Dark'}</div>
-        </div>
-        <div className='mt-10 mr-2'>
-          {/* <div className='text-[#938c82] dark:text-[#938c82] text-2xl text-end font-semibold'>45 X 24</div> */}
-          <div className='text-[#483e2e] dark:text-[#edecea] text-4xl text-end font-bold'>{result}</div>
-        </div>
+          </div>
+        </label>
+        <div className='text-[#8c9298]'>Switch to {toggleDarkMode ? 'Light' : 'Dark'}</div>
       </div>
-      <div className='p-4'>
-        <div className='flex justify-end'>
-          <button onClick={backSpace}>
-            <FiDelete className='h-10 w-10 text-[#f1af60]'/>
-          </button>
+      <div className='p-4 pb-10 absolute bottom-0 w-full z-10'>
+        <div className='mb-10 mr-2 border-b-[0.5px] border-gray-300 dark:border-[#fdfdfd] pb-3'>
+          <div className='dark:text-[#edecea] text-[60px] leading-[71px] text-end font-semibold'>{result}</div>
         </div>
-        <div className='grid grid-cols-4 gap-4 mt-4'>
-          <button onClick={clear} className='dark:text-[#ffffff] bg-[#ffe3aa] dark:bg-[#847d6a] col-span-3 p-5 rounded-md text-xl font-bold'>CLEAR</button>
-          <button onClick={handleClick} name='/' className='dark:text-[#ffffff] bg-[#f1af60] p-5 rounded-md text-xl font-extrabold text-center'>/</button>
-          <button onClick={handleClick} name='7' className='dark:text-[#ffffff] bg-[#f5ce9f] dark:bg-[#533d28] p-5 rounded-md text-xl font-extrabold text-center'>7</button>
-          <button onClick={handleClick} name='8' className='dark:text-[#ffffff] bg-[#f5ce9f] dark:bg-[#533d28] p-5 rounded-md text-xl font-extrabold text-center'>8</button>
-          <button onClick={handleClick} name='9' className='dark:text-[#ffffff] bg-[#f5ce9f] dark:bg-[#533d28] p-5 rounded-md text-xl font-extrabold text-center'>9</button>
-          <button onClick={handleClick} name='*' className='dark:text-[#ffffff] bg-[#f1af60] p-5 rounded-md text-xl font-extrabold text-center'>X</button>
-          <button onClick={handleClick} name='4' className='dark:text-[#ffffff] bg-[#f5ce9f] dark:bg-[#533d28] p-5 rounded-md text-xl font-extrabold text-center'>4</button>
-          <button onClick={handleClick} name='5' className='dark:text-[#ffffff] bg-[#f5ce9f] dark:bg-[#533d28] p-5 rounded-md text-xl font-extrabold text-center'>5</button>
-          <button onClick={handleClick} name='6' className='dark:text-[#ffffff] bg-[#f5ce9f] dark:bg-[#533d28] p-5 rounded-md text-xl font-extrabold text-center'>6</button>
-          <button onClick={handleClick} name='-' className='dark:text-[#ffffff] bg-[#f1af60] p-5 rounded-md text-xl font-extrabold text-center'>-</button>
-          <button onClick={handleClick} name='1' className='dark:text-[#ffffff] bg-[#f5ce9f] dark:bg-[#533d28] p-5 rounded-md text-xl font-extrabold text-center'>1</button>
-          <button onClick={handleClick} name='2' className='dark:text-[#ffffff] bg-[#f5ce9f] dark:bg-[#533d28] p-5 rounded-md text-xl font-extrabold text-center'>2</button>
-          <button onClick={handleClick} name='3' className='dark:text-[#ffffff] bg-[#f5ce9f] dark:bg-[#533d28] p-5 rounded-md text-xl font-extrabold text-center'>3</button>
-          <button onClick={handleClick} name='+' className='dark:text-[#ffffff] bg-[#f1af60] p-5 rounded-md text-xl font-extrabold text-center'>+</button>
-          <button onClick={handleClick} name='0' className='dark:text-[#ffffff] bg-[#f5ce9f] dark:bg-[#533d28] p-5 rounded-md text-xl font-extrabold col-span-2'>0</button>
-          <button onClick={handleClick} name='.' className='dark:text-[#ffffff] bg-[#f5ce9f] dark:bg-[#533d28] p-5 rounded-md text-xl font-extrabold text-center'>.</button>
-          <button onClick={calculate} className='bg-gradient-to-t from-[#f0920a] to-[#fede6f]  p-5 rounded-md text-xl font-extrabold text-center'>=</button>
+        <div className='grid grid-cols-4 gap-5'>
+          <button onClick={clear} className={`text-[#7E83FF] col-span-2 ${!toggleDarkMode ? 'button-light' : 'button-dark'}`}>CLEAR</button>
+          <button onClick={backSpace} className={`${!toggleDarkMode ? 'button-light' : 'button-dark'}`}>
+            <RiDeleteBack2Line className='text-[#7E83FF] m-auto'/>
+          </button>
+          <button onClick={handleClick} name='/' className={`${!toggleDarkMode ? 'button-light' : 'button-dark'} text-[#9ABD0E]`}>/</button>
+          <button onClick={handleClick} name='7' className={`${!toggleDarkMode ? 'button-light' : 'button-dark'} dark:text-white`}>7</button>
+          <button onClick={handleClick} name='8' className={`${!toggleDarkMode ? 'button-light' : 'button-dark'} dark:text-white`}>8</button>
+          <button onClick={handleClick} name='9' className={`${!toggleDarkMode ? 'button-light' : 'button-dark'} dark:text-white`}>9</button>
+          <button onClick={handleClick} name='*' className={`${!toggleDarkMode ? 'button-light' : 'button-dark'} text-[#9ABD0E]`}>x</button>
+          <button onClick={handleClick} name='4' className={`${!toggleDarkMode ? 'button-light' : 'button-dark'} dark:text-white`}>4</button>
+          <button onClick={handleClick} name='5' className={`${!toggleDarkMode ? 'button-light' : 'button-dark'} dark:text-white`}>5</button>
+          <button onClick={handleClick} name='6' className={`${!toggleDarkMode ? 'button-light' : 'button-dark'} dark:text-white`}>6</button>
+          <button onClick={handleClick} name='-' className={`${!toggleDarkMode ? 'button-light' : 'button-dark'} text-[#9ABD0E]`}>-</button>
+          <button onClick={handleClick} name='1' className={`${!toggleDarkMode ? 'button-light' : 'button-dark'} dark:text-white`}>1</button>
+          <button onClick={handleClick} name='2' className={`${!toggleDarkMode ? 'button-light' : 'button-dark'} dark:text-white`}>2</button>
+          <button onClick={handleClick} name='3' className={`${!toggleDarkMode ? 'button-light' : 'button-dark'} dark:text-white`}>3</button>
+          <button onClick={handleClick} name='+' className={`${!toggleDarkMode ? 'button-light' : 'button-dark'} text-[#9ABD0E]`}>+</button>
+          <button onClick={handleClick} name='0' className={`${!toggleDarkMode ? 'button-light' : 'button-dark'} dark:text-white col-span-2`}>0</button>
+          <button onClick={handleClick} name='.' className={`${!toggleDarkMode ? 'button-light' : 'button-dark'} dark:text-white`}>.</button>
+          <button onClick={calculate} className={`${!toggleDarkMode ? 'button-light' : 'button-dark'} text-[#9ABD0E]`}>=</button>
         </div>
       </div>
     </div>
