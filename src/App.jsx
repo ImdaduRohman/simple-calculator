9f5b109f36b862ed229c661a7709adf094577317
ImdaduRohman <imdadu.ylabs1@gmail.com>
2024-01-18 14:37:11 +0700

    feat: handle dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
-import { useState } from 'react';
 import { CiDark, CiLight } from "react-icons/ci";
 import { FiDelete } from "react-icons/fi";
+import { useState } from 'react';
 import './App.css'
 
 function App() {
   const [toggleDarkMode, setToggleDarkMode] = useState();
 
   const handleToggleDarkMode = () => {
-    setToggleDarkMode(!toggleDarkMode)
+    if(toggleDarkMode === false) {
+      // localStorage.setItem('dark', true)
+      document.documentElement.classList.add('dark')
+    } else {
+      // localStorage.setItem('dark', false)
+      document.documentElement.classList.remove('dark')
+    }
+    setToggleDarkMode(!toggleDarkMode);
   }
 
   return (
-    <div className='min-h-screen bg-[#ffffdd]'>
-      <div className='bg-[#fff0d2] p-4 border-b-[4px] border-[#483e2e]'>
-        <div>
+    <div className='min-h-screen bg-[#ffffdd] dark:bg-[#252628]'>
+      <div className='bg-[#fff0d2] dark:bg-[#1b1c1e] p-4 border-b-[4px] border-[#483e2e] dark:border-[#fdfdfd]'>
+        <div className='flex gap-2'>
           <input 
             type='checkbox' 
             id='toggle-dark-mode' 
@@ -29,10 +36,11 @@ function App() {
               </div>
             </div>
           </label>
+          <div className='text-[#8c9298]'>Switch to {toggleDarkMode ? 'Light' : 'Dark'}</div>
         </div>
         <div className='mt-10 mr-2'>
-          <div className='text-[#938c82] text-2xl text-end font-semibold'>45 X 24</div>
-          <div className='text-[#483e2e] text-6xl text-end font-bold'>1080</div>
+          <div className='text-[#938c82] dark:text-[#938c82] text-2xl text-end font-semibold'>45 X 24</div>
+          <div className='text-[#483e2e] dark:text-[#edecea] text-6xl text-end font-bold'>1080</div>
         </div>
       </div>
       <div className='p-4'>
@@ -42,22 +50,22 @@ function App() {
           </button>
         </div>
         <div className='grid grid-cols-4 gap-4 mt-4'>
-          <div className='bg-[#ffe3aa] col-span-3 p-5 rounded-md text-xl font-bold'>CLEAR</div>
-          <div className='bg-[#f1af60] p-5 rounded-md text-xl font-extrabold text-center'>/</div>
-          <div className='bg-[#f5ce9f] p-5 rounded-md text-xl font-extrabold text-center'>7</div>
-          <div className='bg-[#f5ce9f] p-5 rounded-md text-xl font-extrabold text-center'>8</div>
-          <div className='bg-[#f5ce9f] p-5 rounded-md text-xl font-extrabold text-center'>9</div>
-          <div className='bg-[#f1af60] p-5 rounded-md text-xl font-extrabold text-center'>X</div>
-          <div className='bg-[#f5ce9f] p-5 rounded-md text-xl font-extrabold text-center'>4</div>
-          <div className='bg-[#f5ce9f] p-5 rounded-md text-xl font-extrabold text-center'>5</div>
-          <div className='bg-[#f5ce9f] p-5 rounded-md text-xl font-extrabold text-center'>6</div>
-          <div className='bg-[#f1af60] p-5 rounded-md text-xl font-extrabold text-center'>-</div>
-          <div className='bg-[#f5ce9f] p-5 rounded-md text-xl font-extrabold text-center'>1</div>
-          <div className='bg-[#f5ce9f] p-5 rounded-md text-xl font-extrabold text-center'>2</div>
-          <div className='bg-[#f5ce9f] p-5 rounded-md text-xl font-extrabold text-center'>3</div>
-          <div className='bg-[#f1af60] p-5 rounded-md text-xl font-extrabold text-center'>+</div>
-          <div className='bg-[#f5ce9f] p-5 rounded-md text-xl font-extrabold col-span-2'>0</div>
-          <div className='bg-[#f5ce9f] p-5 rounded-md text-xl font-extrabold text-center'>.</div>
+          <div className='dark:text-[#ffffff] bg-[#ffe3aa] dark:bg-[#847d6a] col-span-3 p-5 rounded-md text-xl font-bold'>CLEAR</div>
+          <div className='dark:text-[#ffffff] bg-[#f1af60] p-5 rounded-md text-xl font-extrabold text-center'>/</div>
+          <div className='dark:text-[#ffffff] bg-[#f5ce9f] dark:bg-[#533d28] p-5 rounded-md text-xl font-extrabold text-center'>7</div>
+          <div className='dark:text-[#ffffff] bg-[#f5ce9f] dark:bg-[#533d28] p-5 rounded-md text-xl font-extrabold text-center'>8</div>
+          <div className='dark:text-[#ffffff] bg-[#f5ce9f] dark:bg-[#533d28] p-5 rounded-md text-xl font-extrabold text-center'>9</div>
+          <div className='dark:text-[#ffffff] bg-[#f1af60] p-5 rounded-md text-xl font-extrabold text-center'>X</div>
+          <div className='dark:text-[#ffffff] bg-[#f5ce9f] dark:bg-[#533d28] p-5 rounded-md text-xl font-extrabold text-center'>4</div>
+          <div className='dark:text-[#ffffff] bg-[#f5ce9f] dark:bg-[#533d28] p-5 rounded-md text-xl font-extrabold text-center'>5</div>
+          <div className='dark:text-[#ffffff] bg-[#f5ce9f] dark:bg-[#533d28] p-5 rounded-md text-xl font-extrabold text-center'>6</div>
+          <div className='dark:text-[#ffffff] bg-[#f1af60] p-5 rounded-md text-xl font-extrabold text-center'>-</div>
+          <div className='dark:text-[#ffffff] bg-[#f5ce9f] dark:bg-[#533d28] p-5 rounded-md text-xl font-extrabold text-center'>1</div>
+          <div className='dark:text-[#ffffff] bg-[#f5ce9f] dark:bg-[#533d28] p-5 rounded-md text-xl font-extrabold text-center'>2</div>
+          <div className='dark:text-[#ffffff] bg-[#f5ce9f] dark:bg-[#533d28] p-5 rounded-md text-xl font-extrabold text-center'>3</div>
+          <div className='dark:text-[#ffffff] bg-[#f1af60] p-5 rounded-md text-xl font-extrabold text-center'>+</div>
+          <div className='dark:text-[#ffffff] bg-[#f5ce9f] dark:bg-[#533d28] p-5 rounded-md text-xl font-extrabold col-span-2'>0</div>
+          <div className='dark:text-[#ffffff] bg-[#f5ce9f] dark:bg-[#533d28] p-5 rounded-md text-xl font-extrabold text-center'>.</div>
           <div className='bg-gradient-to-t from-[#f0920a] to-[#fede6f]  p-5 rounded-md text-xl font-extrabold text-center'>=</div>
         </div>
       </div>
