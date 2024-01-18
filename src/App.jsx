50855188a6bcca50bc64e385bd775e34de659613
ImdaduRohman <imdadu.ylabs1@gmail.com>
2024-01-18 11:02:46 +0700

    feat: add dark mode section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,40 @@
+import { useState } from 'react';
+import { CiDark, CiLight } from "react-icons/ci";
 import './App.css'
 
 function App() {
+  const [toggleDarkMode, setToggleDarkMode] = useState();
+
+  const handleToggleDarkMode = () => {
+    setToggleDarkMode(!toggleDarkMode)
+  }
 
   return (
-    <>
-    <h1 className="text-3xl font-bold underline bg-red-500 w-full">
-      Hello world!
-    </h1>
-    </>
+    <div>
+      <div className='bg-[#fff0d2] p-4'>
+        <div>
+          <input 
+            type='checkbox' 
+            id='toggle-dark-mode' 
+            className='hidden'
+            checked={toggleDarkMode || ''}
+            value={toggleDarkMode || ''}
+            onChange={handleToggleDarkMode}
+          />
+          <label htmlFor='toggle-dark-mode'>
+            <div className={`w-[48px] rounded-full p-[1px] cursor-pointer bg-white`}>
+              <div className='w-5 h-5 rounded-full bg-[#f1af60] toggle-circle flex justify-center items-center'>
+                {!toggleDarkMode ? <CiLight className='text-white font-medium' /> : <CiDark className='text-white font-medium' />}
+              </div>
+            </div>
+          </label>
+        </div>
+        <div>
+          <div>Input</div>
+          <div>Result</div>
+        </div>
+      </div>
+    </div>
   )
 }
 
